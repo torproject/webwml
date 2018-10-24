1d03562504884073002907ddf8d981f9bae7610f
traumschule <traumschuleriebau@riseup.net>
2018-10-24 16:17:58 +0200

    merge download pages (#14686)
    - update .htaccess
    - warnings.wmi: recommend to use Pluggable Transports (#25098)
    
    - add slackbuilds (fixes #26307)
    - link to Tor Browser Hacking Guide (#24255)
    - link to OnionBrowser with warning (fixes #26133)
    - tor: add oldstable, testing and beta (#17805, #26298)
    - update links to download page (#27131)
    
    - replace javascript with css accordion (#27540)
    - css: move OS box up, clean indention

--- js/dlpage01.js
+++ js/dlpage01.js
@@ -1,30 +1,9 @@
 $(function(){
-  /* If javascript is enabled this hides all sections by default */
-  $(".easy").css('display', 'none');
   /* Only show language selector if javascript is enabled */
   $('.lang').css('display', 'block');
   $('.lang-alt').css('display', 'none');
-  $('.expander').css('display', 'block');
+  $('.expander').css('height', '100%');
   $('.sidenav').css('display', 'none');
-});
-
-/* Uses result of jquery.client to open the relevant section */
-function OScheck() {
-  var clientos = $.client.os;
-  if(clientos == "Linux"){
-    $('.easy.linux').css('display', 'block');
-  }else if(clientos == "Windows"){
-    $('.easy.windows').css('display', 'block');
-  }else if(clientos == "Mac"){
-      $('.easy.mac').css('display', 'block');
-  }else if(clientos == "Android"){
-    $('.easy.android').css('display', 'block');
-  }else{
-    $('.easy').css('display', 'block');
-  }
-}
-  $(function(){
-  OScheck();
 });
 
 /* Reset language default - needed in the event of returning to the page via back button */
@@ -84,38 +63,4 @@ $(document).ready(function () {
       //goto that anchor by setting the body scroll top to anchor top
 //      $('html, body').animate({scrollTop:target_top}, 1000);
     });
-
-
-    // Bind an event to window.onhashchange
-    $(window).bind( 'hashchange', function(e) {
-
-      // Get the hash (fragment) as a string, with any leading # removed.
-      var url = $.param.fragment();
-
-      // Toggle the '.easy' divs to off
-      if(url == 'windows'|url == 'mac'|url == 'linux'|url == 'openbsd'|url == 'android'){
-
-	  $('.easy').css('display', 'none');
-      }
-
-      if(url == 'windows'){
-	$('.easy.windows').css('display', 'block');
-      } else if(url == 'mac'){
-	$('.easy.mac').css('display', 'block');
-      } else if(url == 'linux'){
-	  $('.easy.linux').css('display', 'block');
-      } else if(url == 'openbsd'){
-	$('.easy.bsd').css('display', 'block');
-      } else if(url == 'android'){
-  $('.easy.android').css('display', 'block');
-      } else {
-	  $('.easy').css('display', 'none');
-	  $(function(){OScheck();});
-      }
-    });
-
-    // Since the event is only triggered when the hash changes, we need to trigger
-    // the event now, to handle the hash the page may have loaded with.
-    $(window).trigger( 'hashchange' );
-
 });
